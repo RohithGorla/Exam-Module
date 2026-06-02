2b7e1207cdce1eaad3b4a336904398487c8add1f
Rohith Gorla <129950040+RohithGorla@users.noreply.github.com>
2026-06-02 10:37:24 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: process.env.REACT_APP_API_URL || '/api' 
+  baseURL: 'https://exam-module-backend.onrender.com/api' 
 });
 
 API.interceptors.request.use((config) => {
